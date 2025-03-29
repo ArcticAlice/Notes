244aa45472197705d297aaab9c6111065f15357c
ArcticAlice <156856574+ArcticAlice@users.noreply.github.com>
2025-03-29 18:08:34 -0400

    Updated Text.py
    First it turns image into a grayscale and then proceeds to increase the contrast. Afterwards the image is sent to the gemini api which then converts the document into latex code.

--- Text.py
+++ Text.py
@@ -1,27 +1,44 @@
-from dotenv import load_dotenv
-
-import google.generativeai as genai
-
 import PIL.Image
+import cv2
+import matplotlib.pyplot as plt
 import os
+
+from google.genai import types
+from google import genai
+from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
 API_KEY = os.getenv("API_KEY")
 
-genai.configure(api_key=API_KEY)
+client = genai.Client(api_key=API_KEY)
 
-model = genai.GenerativeModel('gemini-1.5-flash')
+options = {
+    "seed": 42,
+    "temperature": 0.7
+}
 
-img = PIL.Image.open('something.webp')
+img = cv2.imread("Something.webp") # Read the image
 
-prompt = "Extract text from the image and organize it into ordered blocks. For each block, provide: " \
-         "1. Heading: Average font size of the text (for typed text) or specify 'Handwritten' (for handwritten text). " \
-         "2. Body: Actual text content. " \
-         "Separate blocks based on whether the text is typed or handwritten. " \
-         "Ensure the output maintains structured formatting and accurately categorizes each block."
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Convert the image to grayscale
 
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8)) # Create a CLAHE object (you can adjust clipLimit and tileGridSize as needed)
 
-response = model.generate_content([prompt, img])
+enhanced = clahe.apply(gray)
+
+blurred = cv2.GaussianBlur(enhanced, (3, 3), 0)
+
+prompt = "Given the image, convert the image into latex code. Be sure to use math formating when needed"
+
+threshold_value = 155
+ret, binary = cv2.threshold(enhanced, threshold_value, 255, cv2.THRESH_BINARY)
+
+cv2.imwrite("Enhanced.jpg", binary) # Save the processed image
+
+organ = PIL.Image.open("Enhanced.jpg")
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents=[prompt, organ], config=types.GenerateContentConfig(temperature=0, seed=5)
+)
 
 print(response.text)
