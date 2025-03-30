b9a248a5f925f74530c7abe1ed7737266adb906d
K <redknclarke@gmail.com>
2025-03-30 10:54:08 -0400

    before thingy

--- App.py
+++ App.py
@@ -14,6 +14,7 @@ def selectfile():
     if not valid:
         label.configure(text="Invalid file!")
         chosen_file_path = ""
+        delete_conversion_ui()
         return
 
     file_name = os.path.split(file_path)[1]
@@ -38,12 +39,22 @@ def isFileValid(file_path):
     return os.path.isfile(file_path)
 
 def convert():
-    print(chosen_file_path)
-
     if not len(chosen_file_path) > 0:
         return
     
+    start_conversion_ui()
+    app.update()
     image_to_latex(chosen_file_path, "document.tex")
+    finish_conversion_ui()
+
+def start_conversion_ui():
+    conversion_label.configure(text="Converting...")
+
+def finish_conversion_ui():
+    conversion_label.configure(text="Done!")
+
+def delete_conversion_ui():
+    conversion_label.configure(text="")
 
 # Set up the app
 app = ctk.CTk()
@@ -61,5 +72,8 @@ label.grid(row=0, column=1, padx=10, pady=10)
 
 button2 = ctk.CTkButton(app, 300, 125, text="Convert!", font=font, command=convert)
 button2.grid(row=2, column=0, padx=10, pady=10)
+
+conversion_label = ctk.CTkLabel(app, 300, 25, text="", font=font)
+conversion_label.grid(row=2, column=1, padx=10, pady=10)
 
 app.mainloop()
